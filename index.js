027aa5c10fe5d20b1858a84a7a3cf33e10d14200
Jeremy Warren <jeremy@activatecare.com>
2022-02-15 09:23:35 -0500

    removed css modules

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var css = require('./lib/styles.module.css');
 var smallScreenMin = 768;
 
 // default className for NotificationsSystem component
-var notificationsSystemClassName = css['notifications-system'];
+var notificationsSystemClassName = 'notifications-system';
 
 // default className for NotificationsContainer component
 var notificationsContainerClassName = {
-  main: css['notifications-container'],
+  main: 'notifications-container',
   position: function position(_position) {
-    return css['notifications-container--' + _position];
+    return 'notifications-container--' + _position;
   }
 };
 
@@ -23,40 +23,40 @@ var notificationsContainerTransition = {
   enterTimeout: 500,
   leaveTimeout: 900,
   name: {
-    enter: css['notification-wrapper-enter'],
-    leave: css['notification-wrapper-leave']
+    enter: 'notification-wrapper-enter',
+    leave: 'notification-wrapper-leave'
   }
 };
 
 // default className for Notification component
 var notificationClassName = {
-  main: css['notification'],
-  wrapper: css['notification-wrapper'],
-  meta: css['notification-meta'],
-  title: css['notification-title'],
-  message: css['notification-message'],
+  main: 'notification',
+  wrapper: 'notification-wrapper',
+  meta: 'notification-meta',
+  title: 'notification-title',
+  message: 'notification-message',
   // `fa` corresponds to font-awesome's class name
-  icon: 'fa ' + css['notification-icon'],
-  imageContainer: css['notification-image-container'],
-  image: css['notification-image'],
+  icon: 'fa ' + 'notification-icon',
+  imageContainer: 'notification-image-container',
+  image: 'notification-image',
   status: function status(_status) {
-    return css['notification--' + _status];
+    return 'notification--' + _status;
   },
-  dismissible: css['notification--dismissible'],
+  dismissible: 'notification--dismissible',
   buttons: function buttons(count) {
     if (count === 0) {
       return '';
     } else if (count === 1) {
-      return css['notification--buttons-1'];
+      return 'notification--buttons-1';
     } else if (count === 2) {
-      return css['notification--buttons-2'];
+      return 'notification--buttons-2';
     }
-    return css['notification-buttons'];
+    return 'notification-buttons';
   },
-  closeButtonContainer: css['notification-close-button-container'],
-  closeButton: 'fa ' + css['notification-close-button'],
-  button: css['notification-button'],
-  buttonText: css['notification-button-text']
+  closeButtonContainer: 'notification-close-button-container',
+  closeButton: 'fa ' + 'notification-close-button',
+  button: 'notification-button',
+  buttonText: 'notification-button-text'
 };
 
 module.exports = {
